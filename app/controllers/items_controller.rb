5a377d51e3e6cf8ac0354bc522fe9e963bbed4f4
akihiro mochizuki <a.mochizuki13@gmail.com>
2017-04-23 21:52:23 +0000

    rakuten api

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,36 @@
+class ItemsController < ApplicationController
+  def new
+    @items = []
+    
+    @keyword = params[:keyword]
+    if @keyword
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits: 20,
+      })
+      
+      results.each do |result|
+        # 扱いやすいようにItemとしてインスタンスを作成する。（保存はしない。）
+        item = Item.new(read(result))
+        @items << item
+      end
+    end
+  end
+  
+  private
+  
+  def read(result)
+    code = result.code
+    name = result["itemName"]
+    url = result.url
+    image_url = result["mediumImageUrls"].first["imageUrl"].gsub("?_ex=128x128","")
+    
+    return {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
+end
